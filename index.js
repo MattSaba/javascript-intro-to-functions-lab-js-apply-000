17698099619d084a7ad009f1c889ea69f42f5f63
Matt Saba <mattsaba@me.com>
2018-09-27 23:43:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ function logWhisper(string)
 function sayHiToGrandma(string) 
 {
   if(string === string.toLowerCase()) {
-    console.log("I can\'t hear you!")
+    return ("I can\'t hear you!")
   }
   if(string === string.toUpperCase()) {
     console.log("YES INDEED!")
